39723cbfe0478e337224b51ac5e44cec09067d95
Alex Rodriguez <alexrodriguez0719@gmail.com>
2020-05-20 16:25:06 -0500

    5/20

--- scripts/functions.js
+++ scripts/functions.js
@@ -7,16 +7,34 @@ var imageHover = function () {
 
  var imageClick = function () {
      var $this = $(this);
-     var newSource = $this.data('open');
-     $this.data('open', $this.attr('src'));
-     $this.attr('src', newSource);
+     if ($this.attr('src').substring(9) == 'open.gif'){
+        var letters = $this.attr('src').substring(7,9);
+        switch (letters){
+            case 'gt':
+                window.open("https://github.com/arod0719");
+                break;
+            case 'ln':
+                window.open("https://www.linkedin.com/in/alexander-rodriguez-7278b2182/");
+                break;
+            case 'rs':
+                 window.open('./resources/AlexRodriguezResume.pdf');
+                break;
+        }
+     }
+     else{
+         var newSource = $this.data('open');
+         $this.data('open', $this.attr('src'));
+         $this.attr('src', newSource);
+     }
  }
 
  var imageRevert = function () {
       var $this = $(this);
-      $this.data('wiggle', 'images/wiggle.gif');
       $this.data('static', 'images/static.png');
-      $this.data('open', 'images/open.gif');
+      if ($this.data('wiggle').substring(9) == 'open.gif'){
+        $this.data('open', $this.data('wiggle'));
+        $this.data('wiggle', 'images/wiggle.gif');
+       }
   }
 
 $(function () {
